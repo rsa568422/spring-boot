f9ad557d02bb41c87c94b2d6d8308326d394b710
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-26 19:16:03 +0200

    Creando nuevo proyecto y controlador

---
+++ springboot-di/src/main/java/udemy/springboot/di/app/SpringbootDiApplication.java
@@ -0,0 +1,13 @@
+package udemy.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiApplication.class, args);
+	}
+
+}
---
+++ springboot-di/src/test/java/udemy/springboot/di/app/SpringbootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
