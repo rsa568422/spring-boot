ffb7a6a8302e6874afe8e853aef6e602d52e2fe8
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-26 17:08:27 +0200

    Obtener varios parámetros de la ruta con @PathVariable

--- springboot-web/src/main/java/udemy/springboot/web/app/controllers/PathVariableExample.java
+++ springboot-web/src/main/java/udemy/springboot/web/app/controllers/PathVariableExample.java
@@ -10,10 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class PathVariableExample {
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Sending path variables");
+        return "variables/index";
+    }
+
     @GetMapping("/string/{text}")
     public String variables(@PathVariable String text, Model model) {
         model.addAttribute("title", "Receiving path variables");
         model.addAttribute("result", String.format("Sent text is: %s", text));
+        return "variables/view";
+    }
+
+    @GetMapping("/string/{text}/{number}")
+    public String variables(@PathVariable String text, @PathVariable Integer number, Model model) {
+        model.addAttribute("title", "Receiving path variables");
+        model.addAttribute("result", String.format("Sent text is: %s | Sent number is: %d", text, number));
         return "variables/view";
     }
 }
