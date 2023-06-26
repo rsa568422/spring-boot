633224575d362e39bfbe26ff0ab1eadac5ed709b
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-27 00:23:21 +0200

    Añadiendo la clase de servicio

--- springboot-di/src/main/java/udemy/springboot/di/app/controllers/IndexController.java
+++ springboot-di/src/main/java/udemy/springboot/di/app/controllers/IndexController.java
@@ -3,12 +3,16 @@ package udemy.springboot.di.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import udemy.springboot.di.app.models.services.MyService;
 
 @Controller
 public class IndexController {
 
+    private MyService service = new MyService();
+
     @GetMapping({"", "/", "/index"})
     public String index(Model model) {
+        model.addAttribute("result", service.operation());
         return "index";
     }
 }
---
+++ springboot-di/src/main/java/udemy/springboot/di/app/models/services/MyService.java
@@ -0,0 +1,8 @@
+package udemy.springboot.di.app.models.services;
+
+public class MyService {
+
+    public String operation() {
+        return "ejecutando proceso de modelo de negocio";
+    }
+}
