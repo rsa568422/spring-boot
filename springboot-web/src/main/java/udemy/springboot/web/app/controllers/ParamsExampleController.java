35591ce1425ddea2be7e065911e6bc55076b9bce
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-26 16:50:49 +0200

    Obtener varios parámetros de la URL con @RequestParam

--- springboot-web/src/main/java/udemy/springboot/web/app/controllers/ParamsExampleController.java
+++ springboot-web/src/main/java/udemy/springboot/web/app/controllers/ParamsExampleController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class ParamsExampleController {
@@ -18,6 +20,25 @@ public class ParamsExampleController {
     @GetMapping("/string")
     public String param(@RequestParam(name = "text", required = false) String text, Model model) {
         model.addAttribute("result", String.format("Sent text is: %s", text));
+        return "params/view";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String text, @RequestParam Integer number, Model model) {
+        model.addAttribute("result", String.format("Sent text is: %s | Sent number is: %d", text, number));
+        return "params/view";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+        String text = request.getParameter("text");
+        int number;
+        try {
+            number = Integer.parseInt(request.getParameter("number"));
+        } catch (NumberFormatException e) {
+            number = 0;
+        }
+        model.addAttribute("result", String.format("Sent text is: %s | Sent number is: %d", text, number));
         return "params/view";
     }
 }
