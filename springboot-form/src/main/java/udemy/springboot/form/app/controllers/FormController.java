d758d634fe2d8b7c7784a531caaaa587eac0968a
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-27 18:48:00 +0200

    Validación del formulario usando anotaciones

--- springboot-form/src/main/java/udemy/springboot/form/app/controllers/FormController.java
+++ springboot-form/src/main/java/udemy/springboot/form/app/controllers/FormController.java
@@ -2,9 +2,14 @@ package udemy.springboot.form.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import udemy.springboot.form.app.models.entities.User;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -16,8 +21,18 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String process(User user, Model model) {
+    public String process(@Valid User user, BindingResult result, Model model) {
         model.addAttribute("title", "Form Resultado");
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(error -> errors.put(error.getField(), String.format(
+                    "The field %s have error: %s",
+                    error.getField(),
+                    error.getDefaultMessage()
+            )));
+            model.addAttribute("errors", errors);
+            return "form";
+        }
         model.addAttribute("user", user);
         return "result";
     }
--- springboot-form/src/main/java/udemy/springboot/form/app/models/entities/User.java
+++ springboot-form/src/main/java/udemy/springboot/form/app/models/entities/User.java
@@ -5,15 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
+    @NotEmpty
     private String username;
 
+    @NotEmpty
     private String password;
 
+    @NotEmpty
     private String email;
 }
