0a0bbe6d8b59999d09909677dd689b1795a5d715
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-27 10:40:27 +0200

    Creando nuevo proyecto form

---
+++ springboot-form/src/main/java/udemy/springboot/form/app/controllers/FormController.java
@@ -0,0 +1,20 @@
+package udemy.springboot.form.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String process(Model model) {
+        return "result";
+    }
+}
---
+++ springboot-form/src/test/java/udemy/springboot/form/app/SpringbootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.springboot.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
