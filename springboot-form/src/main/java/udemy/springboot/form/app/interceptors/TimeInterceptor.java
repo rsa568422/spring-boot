dc2e90055e794cccb947e4f651de7d9a1aeb6278
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-06-29 18:16:54 +0200

    Implementando interceptor TiempoTranscurrido

--- springboot-form/src/main/java/udemy/springboot/form/app/interceptors/TimeInterceptor.java
+++ springboot-form/src/main/java/udemy/springboot/form/app/interceptors/TimeInterceptor.java
@@ -1,19 +1,29 @@
 package udemy.springboot.form.app.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.SecureRandom;
+import java.util.Objects;
 
 @Component
 public class TimeInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(TimeInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws Exception {
+        log.info("TimeInterceptor: preHandle(...)");
+        long init = System.currentTimeMillis();
+        request.setAttribute("init", init);
+        Thread.sleep(SecureRandom.getInstanceStrong().nextInt(500));
         return true;
     }
 
@@ -22,6 +32,13 @@ public class TimeInterceptor implements HandlerInterceptor {
                            HttpServletResponse response,
                            Object handler,
                            ModelAndView modelAndView) throws Exception {
-
+        long init = (Long) request.getAttribute("init");
+        long end = System.currentTimeMillis();
+        long time = end - init;
+        if (Objects.nonNull(modelAndView)) {
+            modelAndView.addObject("time", time);
+        }
+        log.info("Time: {}", time);
+        log.info("TimeInterceptor: postHandle(...)");
     }
 }
