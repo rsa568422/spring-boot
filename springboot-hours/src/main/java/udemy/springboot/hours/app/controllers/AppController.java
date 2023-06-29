be8bd542bdc71663e6929c8d87abb8a5d696849e
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-06-29 23:38:38 +0200

    Creando método handler cerrado

--- springboot-hours/src/main/java/udemy/springboot/hours/app/controllers/AppController.java
+++ springboot-hours/src/main/java/udemy/springboot/hours/app/controllers/AppController.java
@@ -1,5 +1,6 @@
 package udemy.springboot.hours.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
+    @Value("${config.hours.open}")
+    private Integer open;
+
+    @Value("${config.hours.close}")
+    private Integer close;
+
     @GetMapping({"/", "/index"})
     public String index(Model model) {
         model.addAttribute("title", "Bienvenido al horario de atención a clientes");
         return "index";
+    }
+
+    @GetMapping("/close")
+    public String close(Model model) {
+        String message = String.format("Cerrado, por favor visítenos entre las %d y las %d. Gracias.", open, close);
+        model.addAttribute("title", "Fuera del horario de atención");
+        model.addAttribute("message", message);
+        return "close";
     }
 }
