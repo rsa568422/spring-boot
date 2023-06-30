f1d0e55a4e1f2ce7c7fcad17dc6473023f4f108c
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-06-30 17:37:19 +0200

    Creando la clase Entidad JPA anotada con @Entity

---
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/models/entities/Customer.java
@@ -0,0 +1,34 @@
+package udemy.springboot.datajpa.app.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+}
