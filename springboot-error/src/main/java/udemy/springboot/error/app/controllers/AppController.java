bf1016a08397c180252316da89962ba82a90012d
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-06-30 14:51:46 +0200

    Añadiendo método handler ver

--- springboot-error/src/main/java/udemy/springboot/error/app/controllers/AppController.java
+++ springboot-error/src/main/java/udemy/springboot/error/app/controllers/AppController.java
@@ -1,14 +1,34 @@
 package udemy.springboot.error.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import udemy.springboot.error.app.model.entities.User;
+import udemy.springboot.error.app.services.UserService;
 
 @Controller
 public class AppController {
+
+    private final UserService service;
+
+    @Autowired
+    public AppController(UserService service) {
+        this.service = service;
+    }
 
     @GetMapping({"", "/", "/index"})
     public String index() {
         Integer i = (100 / 0) * Integer.parseInt("jhgf");
         return "index";
+    }
+
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable Integer id, Model model) {
+        User user = service.findById(id);
+        model.addAttribute("title", String.format("Detalles del usuario: %s", user.getName()));
+        model.addAttribute("user", user);
+        return "view";
     }
 }
