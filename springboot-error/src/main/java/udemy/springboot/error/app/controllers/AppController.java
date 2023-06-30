c682e375f3833c6584b04054a039f6fa027500be
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-06-30 02:13:28 +0200

    Creando nueva aplicación y controlador

---
+++ springboot-error/src/main/java/udemy/springboot/error/app/controllers/AppController.java
@@ -0,0 +1,13 @@
+package udemy.springboot.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping({"", "/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
