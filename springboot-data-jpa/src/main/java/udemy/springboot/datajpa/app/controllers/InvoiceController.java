89d3e8ec2f0cb47fbf24183597437d85ce82a968
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-03 17:07:45 +0200

    Creando controlador FacturaController con la acción handler crear

---
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/controllers/InvoiceController.java
@@ -0,0 +1,42 @@
+package udemy.springboot.datajpa.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import udemy.springboot.datajpa.app.models.entities.Customer;
+import udemy.springboot.datajpa.app.models.entities.Invoice;
+import udemy.springboot.datajpa.app.models.services.CustomerService;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/invoice")
+@SessionAttributes("invoice")
+public class InvoiceController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public InvoiceController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/form/{customerId}")
+    public String create(@PathVariable(value = "customerId") Long customerId, Model model, RedirectAttributes flash) {
+        Customer customer = customerService.findOne(customerId);
+        if (Objects.isNull(customer)) {
+            flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+            return "redirect:/list";
+        }
+        Invoice invoice = new Invoice();
+        invoice.setCustomer(customer);
+        model.addAttribute("title", "Crear factura");
+        model.addAttribute("invoice", invoice);
+        return "invoices/form";
+    }
+}
