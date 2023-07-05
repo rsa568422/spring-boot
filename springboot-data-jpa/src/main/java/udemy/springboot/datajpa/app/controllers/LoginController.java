f1e8633e5ca1a3fca0a0ff81068e80435ba07510
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-05 18:54:19 +0200

    Validando LoginController con los mensajes de error

--- springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/controllers/LoginController.java
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package udemy.springboot.datajpa.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -12,10 +13,14 @@ import java.util.Objects;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash) {
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        Model model, Principal principal, RedirectAttributes flash) {
         if (Objects.nonNull(principal)) {
             flash.addFlashAttribute("info", "Ya hay una sesión activa");
             return "redirect:/";
+        }
+        if (Objects.nonNull(error)) {
+            model.addAttribute("error", "nombre de usuario o contraseña incorrectos");
         }
         return "login";
     }
