f06eef23df912b24b62cef9e089a90d3df4fe529
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-05 15:07:31 +0200

    Implementando la clase SpringSecurityConfig para registrar usuarios

---
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/SpringSecurityConfig.java
@@ -0,0 +1,56 @@
+package udemy.springboot.datajpa.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()throws Exception{
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User
+                .withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build());
+        manager.createUser(User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN","USER")
+                .build());
+
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+                .antMatchers("/ver/**").hasAnyRole("USER")
+                .antMatchers("/uploads/**").hasAnyRole("USER")
+                .antMatchers("/form/**").hasAnyRole("ADMIN")
+                .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                .antMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+
+        return http.build();
+    }
+
+}
