a5d5681c42f61ee58dcac3ce96cdd671b19e01b3
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-07 19:20:47 +0200

    Manejo de errores de acceso denegado AccessDeniedPage

--- springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/SpringSecurityConfig.java
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/SpringSecurityConfig.java
@@ -48,7 +48,9 @@ public class SpringSecurityConfig {
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and()
-                .logout().permitAll();
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error_403");
 
         return http.build();
     }
