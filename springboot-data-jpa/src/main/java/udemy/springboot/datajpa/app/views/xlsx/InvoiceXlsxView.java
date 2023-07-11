97d14fc70e4a67ffdbaa0aa36f8d35ed2a2b6693
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-11 20:02:52 +0200

    Configuraciones necesarias y dependencias

---
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/views/xlsx/InvoiceXlsxView.java
@@ -0,0 +1,19 @@
+package udemy.springboot.datajpa.app.views.xlsx;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component("invoice/view.xlsx")
+public class InvoiceXlsxView extends AbstractXlsxView {
+
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook,
+                                      HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    }
+}
