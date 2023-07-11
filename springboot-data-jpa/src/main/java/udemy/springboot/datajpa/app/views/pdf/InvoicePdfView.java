f170e83413111fc1d56f1fdda99da29394880890
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2023-07-11 18:47:54 +0200

    Creando la vista PdfView para la factura

---
+++ springboot-data-jpa/src/main/java/udemy/springboot/datajpa/app/views/pdf/InvoicePdfView.java
@@ -0,0 +1,36 @@
+package udemy.springboot.datajpa.app.views.pdf;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+import udemy.springboot.datajpa.app.models.entities.Invoice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component("invoices/view")
+public class InvoicePdfView extends AbstractPdfView {
+
+    @Override
+    protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
+                                    HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Invoice invoice = (Invoice) model.get("invoice");
+
+        PdfPTable customerTable = new PdfPTable(1);
+        customerTable.addCell("Datos del cliente");
+        customerTable.addCell(String.format("%s %s", invoice.getCustomer().getName(), invoice.getCustomer().getSurname()));
+        customerTable.addCell(invoice.getCustomer().getEmail());
+
+        PdfPTable invoiceTable = new PdfPTable(1);
+        customerTable.addCell("Datos de la factura");
+        customerTable.addCell(String.format("Factura: %s", invoice.getId()));
+        customerTable.addCell(String.format("Descripción: %s", invoice.getDescription()));
+        customerTable.addCell(String.format("Fecha: %s", invoice.getCreateAt()));
+
+        document.add(customerTable);
+        document.add(invoiceTable);
+    }
+}
